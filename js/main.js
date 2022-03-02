223b4d69950f7daf6bf6f00b80075c14120a84f1
arunabhnewar <newardevil@gmail.com>
2022-03-02 20:15:57 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -19,39 +19,130 @@
         const inputsData = [...this.elements];
 
         let formData = {};
+        let isValid = true;
+
         inputsData.forEach(element => {
             if (element.type !== 'submit') {
+                if (element.value == '') {
+                    alert("Please fill up this fields with valid value.");
+                    isValid = false;
+                    return;
+                }
                 formData[element.name] = element.value;
             }
         })
-        formData.status = "Incomplete";
-        showDataOnUI(formData);
+        if (isValid) {
+            formData.status = "Incomplete";
+            formData.id = uuidv4();
+            const lists = getFromLocalStorage();
+            showDataOnUI(formData, lists.length + 1);
+            lists.push(formData);
+            setFromLocalStorage(lists);
+        }
         this.reset();
     })
 
 
     // Showing the tasks list on UI
-    function showDataOnUI(task) {
+    function showDataOnUI({ id, name, priority, status, date }, index) {
         const tr = document.createElement('tr');
         tr.innerHTML = `
-        <td>2</td>
-        <td>${task.name}</td>
-        <td>${task.priority}</td>
-        <td>${task.status}</td>
-        <td>${task.date}</td>
-        <td>
-        <button class="edit">
+        <td id="number">${index}</td>
+        <td id="name">${name}</td>
+        <td id="priority">${priority}</td>
+        <td id="status">${status}</td>
+        <td id="date">${date}</td>
+        <td id="action">
+        <button id="edit">
         <i class="fas fa-pen-square"></i>
         </button>
-        <button class="complete">
+        <button id="check">
         <i class="fas fa-check-circle"></i>
         </button>
-        <button class="delete">
+        <button id="delete">
         <i class="fas fa-trash"></i>
         </button>
         </td>
         `
-        tableBody.appendChild(tr)
+        tr.dataset.id = id;
+        tableBody.appendChild(tr);
+    }
+
+
+    // Get data from local storage
+    function getFromLocalStorage() {
+        let lists = [];
+        const data = localStorage.getItem('lists');
+        if (data) {
+            lists = JSON.parse(data);
+        }
+        return lists;
+    }
+
+
+    // Set data from local storage
+    function setFromLocalStorage(lists) {
+        localStorage.setItem('lists', JSON.stringify(lists));
+    }
+
+
+    // Task list event
+    tableBody.addEventListener('click', function (e) {
+        if (e.target.id == "edit") {
+            console.log("edited");
+        }
+
+        else if (e.target.id == "check") {
+            const tr = e.target.parentElement.parentElement;
+            const id = tr.dataset.id;
+            const tds = tr.children;
+            let lists = getFromLocalStorage();
+
+            const table_data = [...tds];
+            table_data.forEach(td => {
+                if (td.id === "status") {
+                    lists.forEach(list => {
+                        if (list.status == "Incomplete") {
+                            list.status = "Complete";
+                            td.innerHTML = "Complete";
+                        }
+                        else {
+                            list.status = "Incomplete";
+                            td.innerHTML = "Incomplete";
+                        }
+                    })
+                }
+            })
+            setFromLocalStorage(lists);
+        }
+
+        else if (e.target.id == "delete") {
+            const tr = e.target.parentElement.parentElement;
+            const id = tr.dataset.id;
+            tr.remove();
+            let lists = getFromLocalStorage();
+            lists = lists.filter((list) => {
+                if (list.id !== id) {
+                    return list;
+                }
+            })
+            setFromLocalStorage(lists);
+            loading();
+
+        }
+    })
+
+
+
+    // Get tasks on load
+    window.onload = loading();
+
+    function loading() {
+        tableBody.innerHTML = '';
+        const lists = getFromLocalStorage();
+        lists.forEach((list, index) => {
+            showDataOnUI(list, index + 1)
+        })
     }
 
 
